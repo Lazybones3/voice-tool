a5be3a5cf58dd84bce309134c764d97a746f5b18
Lazybones3 <caorui452@163.com>
2026-06-13 11:55:50 +0800

    update audio

--- audio.py
+++ audio.py
@@ -1,39 +1,39 @@
+import argparse
 import os
-import sys
 import re
 import ffmpeg
+import logging
 
 
-def video_to_audio(input_file: str):
+def video_to_audio(input_file: str, prefix: str):
     stream = ffmpeg.input(input_file)
     filename = os.path.basename(input_file)
     try:
         pos = filename.rindex('.')
-        output_file = './out/' + filename[0:pos] + '.mp3'
+        output_file = f'./out/{prefix + filename[0:pos]}.mp3'
         stream = ffmpeg.output(stream, output_file)
         ffmpeg.run(stream)
     except ValueError:
-        print("filename error: " + filename)
-        sys.exit(1)
+        logging.error("filename error: " + filename)
 
 def is_video(filename: str):
     pattern = r'.*\.(mp4|avi|flv|mov|mkv)$'
     return re.match(pattern, filename, flags=re.IGNORECASE)
 
-def load_dir(input_dir: str):
+def load_dir(input_dir: str, prefix: str):
     for dirpath, _, filenames in os.walk(input_dir):
-        print(f"Current directory: {dirpath}")
+        logging.info(f"Current directory: {dirpath}")
         for filename in filenames:
             if is_video(filename):
                 full_filename = os.path.join(dirpath, filename)
-                video_to_audio(full_filename)
+                video_to_audio(full_filename, prefix)
 
 def main():
-    if len(sys.argv) != 2:
-        print('Usage: python audio.py <input_dir>')
-        return
-    print('[Convert] ' + sys.argv[1])
-    load_dir(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', help='Input directory')
+    parser.add_argument('--prefix', help='filename prefix')
+    args = parser.parse_args()
+    load_dir(args.input_dir, args.prefix)
 
 
 if __name__ == '__main__':
